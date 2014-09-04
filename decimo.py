3838e44e9274bd1a4ad7d870d52a73f7fc439dc9
cristian <dos_cristian@live.com>
2014-09-05 01:33:06 +0200

    ejercicio6

---
+++ decimo.py
@@ -0,0 +1,21 @@
+a =  input("inserte dato: ")
+n = 1
+m = 1
+o = 0
+while n <= a:
+	m = 1
+	o = n
+	while m <= n:
+		print o,
+		m+=1
+	print
+	n+=1
+n = n -1
+while n >= 1:
+	m = 1
+	o = n
+	while m <= n:
+		print o,
+		m+=1
+	print
+	n-=1
